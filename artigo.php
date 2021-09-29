5acf88dd78098b708c2936667f86cf7c5b471f75
Yan Ueno <yan.ueno@vmarket.com>
2021-09-28 21:30:40 -0300

    first commit

---
+++ artigo.php
@@ -0,0 +1,32 @@
+<?php
+require 'config.php';
+include 'src/Arquivo.php';
+
+$obj_artigo = new Artigo($mysql);
+$artigos =  $obj_artigo->encontrarPorId($_GET['id']);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>
+            <?php echo $artigos['titulo']; ?>
+        </h1>
+        <p>
+        <?php echo $artigos['conteudo']; ?>
+        </p>
+        <div>
+            <a class="botao botao-block" href="index.php">Voltar</a>
+        </div>
+    </div>
+</body>
+
+</html>
